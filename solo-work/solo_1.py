4735a73d9938cb5dfc17f59757d98196af612e93
Anna Nawrocka <77083@student.ue.poznan.pl>
2023-05-09 22:10:19 +0200

    zadanie 1.3

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -9,3 +9,9 @@ print("{} {}".format(hello, student))
 
 student = input("Wpisz swoje imię: ")
 print("Hello {}".format(student))
+
+#zadanie 1.3
+
+studenci = ["Basia", "Kasia", "Zosia", "Ola"]
+liczba_studentow = len(studenci)
+print("Liczba studentów: ", liczba_studentow)
