ed5600449cc7b80712d1c6acca9889aa7e5b191f
Anna Nawrocka <77083@student.ue.poznan.pl>
2023-05-10 17:25:31 +0200

    zadanie 1.5

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -23,4 +23,14 @@ studenci = ["Ania", "Kasia", "Piotr", "Tomek"]
 for student in studenci:
     print("Hello {}".format(student))
 
+#zadanie 1.5
+
+liczba = 3
+potega = 4
+
+wynik = liczba ** potega
+
+print("Wynik wynosi: {}".format(wynik))
+
+
 
